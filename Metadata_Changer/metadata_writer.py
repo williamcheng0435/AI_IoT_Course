9a285e4088d9f1d8d3e1a2750dd537a4f0650b9a
williamchneg0435 <chengyatlong0435@gmail.com>
2022-03-28 21:05:32 +0800

    edit directory

---
+++ Metadata_Changer/metadata_writer.py
@@ -0,0 +1,24 @@
+from tflite_support.metadata_writers import object_detector
+from tflite_support.metadata_writers import writer_utils
+from tflite_support import metadata
+
+ObjectDetectorWriter = object_detector.MetadataWriter
+# _MODEL_PATH = "ssd_mobilenet_v1_1_default_1.tflite"
+# _LABEL_FILE = "labelmap.txt"
+# _SAVE_TO_PATH = "ssd_mobilenet_v1_1_default_1_metadata.tflite"
+
+
+_MODEL_PATH = "detect.tflite"
+_LABEL_FILE = "labels.txt"
+_SAVE_TO_PATH = "detect_test.tflite"
+
+writer = ObjectDetectorWriter.create_for_inference(
+    writer_utils.load_file(_MODEL_PATH), [127.5], [127.5], [_LABEL_FILE])
+writer_utils.save_file(writer.populate(), _SAVE_TO_PATH)
+
+# Verify the populated metadata and associated files.
+displayer = metadata.MetadataDisplayer.with_model_file(_SAVE_TO_PATH)
+print("Metadata populated:")
+print(displayer.get_metadata_json())
+print("Associated file(s) populated:")
+print(displayer.get_packed_associated_file_list())
